214773103cbe6a5eab13474b9a1ce8d643c9c35f
Ahmed Hani Ibrahim <ahani.fcis@gmail.com>
2013-08-24 17:35:11 +0200

    Graph - All-Pairs Shortest Paths - Floyd-Warshall

--- UVa/10171 - MeetingProf.cpp
+++ UVa/10171 - MeetingProf.cpp
@@ -14,110 +14,74 @@
 #include<functional>
 #define Max 26
 #define INF 1000000000
-#define INT_MAX 2147483647
-#define FOR(i , Case) for( int i = 0 ; i < Case ; i++ )
+//#define INT_MAX 2147483647
+#define FOR(i , Case) for(int i = 0; i < Case; i++)
 
 using namespace std;
 
-struct Result{
-    int Energy ;
-    int Distance ;
-    Result() : Distance(INF){}
-   
+struct Result {
+    int Energy;
+    int Distance;
+    Result() : Distance(INF) {}
 };
 
 int Case ,IntialEnergy;
 char First, Second, X, Y;
-int DistinationY[Max][Max] , DistinationM[Max][Max];
+int DistanceY[Max][Max] , DistanceM[Max][Max];
 
 int main(int argc, char** argv) {
-//    freopen("Trojan.txt", "r", stdin);
-    
-      while ( scanf("%d" , &Case)){
-          if ( Case == 0 )
-              break;
-          
-         
-          FOR(i , Max){
-              FOR(j , Max){
-                  DistinationY[i][j] = INF;
-                  DistinationM[i][j] = INF;
-              }
-          }
-          
-          
-          FOR(i , Case){
+     //freopen("Trojan.txt", "r", stdin);
+     
+      while(scanf("%d" , &Case) == 1, Case) {
+          FOR(i, Max) FOR(j, Max) DistanceY[i][j] = INF, DistanceM[i][j] = INF;
+       
+          FOR(i, Case) {
               scanf("%s %s %s %s %d" , &First , &Second , &X , &Y , &IntialEnergy);
-              
-                      
-              if (First == 'Y' ){
-                  if ( Second == 'U')
-                      DistinationY[ X - 'A'][ Y - 'A'] = IntialEnergy;
-                  
-                  else if ( Second == 'B'){
-                      DistinationY [ X - 'A' ][ Y - 'A' ] = IntialEnergy;
-                      DistinationY [ Y - 'A' ][ X - 'A' ] = IntialEnergy;
+              if (First == 'Y'){
+                  if (Second == 'U')
+                      DistanceY[X - 'A'][Y - 'A'] = IntialEnergy; 
+                  else if(Second == 'B') {
+                      DistanceY [X - 'A'][Y - 'A'] = IntialEnergy;
+                      DistanceY [Y - 'A'][X - 'A'] = IntialEnergy;
                   }
-              }
-          
-          
-          else if ( First == 'M' ){
-                  if ( Second == 'U')
-                      DistinationM[ X - 'A'][ Y - 'A'] = IntialEnergy;
-                  
-                  else if ( Second == 'B'){
-                      DistinationM [ X - 'A' ][ Y - 'A' ] = IntialEnergy;
-                      DistinationM [ Y - 'A' ][ X - 'A' ] = IntialEnergy;
+              } else if (First == 'M') {
+                  if (Second == 'U')
+                      DistanceM[X - 'A'][Y - 'A'] = IntialEnergy;
+                  else if (Second == 'B') {
+                      DistanceM [X - 'A'][Y - 'A'] = IntialEnergy;
+                      DistanceM [Y - 'A'][X - 'A'] = IntialEnergy;
                   }
               }
           }
           
-          FOR(i , Max){
-              DistinationY[i][i] = 0;
-              DistinationM[i][i] = 0;
-          }
+          FOR(i, Max) DistanceY[i][i] = 0, DistanceM[i][i] = 0;
           
-          
-          FOR(k , Max){
-              FOR(i , Max){
-                  FOR(j , Max){
-                      DistinationY[i][j] = min( DistinationY[i][j] ,DistinationY[i][k] + DistinationY[k][j] );  
-                      DistinationM[i][j] = min( DistinationM[i][j] ,DistinationM[i][k] + DistinationM[k][j] );
+          FOR(k, Max){
+              FOR(i, Max){
+                  FOR(j, Max){
+                      DistanceY[i][j] = min(DistanceY[i][j], DistanceY[i][k] + DistanceY[k][j]);  
+                      DistanceM[i][j] = min(DistanceM[i][j], DistanceM[i][k] + DistanceM[k][j]);
                   }
               }
           }
-            
-              scanf("%s %s" , &First , &Second);
-              
-              int YY = First - 'A';
-              int MM = Second - 'A';
-              Result Answer;
-              
-              FOR(i , Max){
-                  if ( DistinationY[YY][i] == INF ||  DistinationM[MM][i] == INF )
-                      continue;
-                  
-                  if ( DistinationY[YY][i] +  DistinationM[MM][i] < Answer.Distance){
+          scanf("%s %s" , &First , &Second);
+          int YY = First - 'A';
+          int MM = Second - 'A';
+          Result Answer; 
+          FOR(i , Max) {
+              if(DistanceY[YY][i] == INF || DistanceM[MM][i] == INF) continue;
+              if(DistanceY[YY][i] +  DistanceM[MM][i] < Answer.Distance) {
                       Answer.Energy = i;
-                      Answer.Distance = DistinationY[YY][i] +  DistinationM[MM][i];
-                  }
+                      Answer.Distance = DistanceY[YY][i] +  DistanceM[MM][i];
               }
-              
-              if ( Answer.Distance == INF )
-                  printf("You will never meet.\n");
-              
-              else{
-                  printf("%d" , Answer.Distance);
-                  FOR(i , Max){
-                      if (DistinationY[YY][i] +  DistinationM[MM][i] == Answer.Distance)
-                          printf(" %c" , char(i+'A'));
-                  
-                  }
-                  printf("\n");
-              }
-              
+          }
+          if (Answer.Distance == INF) printf("You will never meet.\n");
+          else {
+              printf("%d" , Answer.Distance);
+              FOR(i, Max)  if(DistanceY[YY][i] +  DistanceM[MM][i] == Answer.Distance) printf(" %c" , char(i + 'A'));
+                  puts("");
+          }
       }
-      
     return 0;
 }
-             
+    
