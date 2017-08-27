abb18f01f1cadcb13a3fe7869b2eac9e5b8745bb
Ahmed Hani Ibrahim <ahani.fcis@gmail.com>
2017-08-27 02:55:23 +0200

    Basic statistics warmup solved

---
+++ Hackerrank/AI/Statistics-MachineLearning/basic_statistics_warmup.py
@@ -0,0 +1,36 @@
+___author__ = 'Ahmed Hani Ibrahim'
+
+import copy as cp
+from collections import Counter
+import math
+
+n = int(input())
+array = list(map(lambda v: float(v), str(input()).strip().split()))
+
+sorted_array = cp.copy(array)
+sorted_array.sort()
+
+mean = sum(array) / float(len(array))
+print(round(mean, 1))
+
+median = sorted_array[len(sorted_array) / 2] if len(sorted_array) % 2 != 0 else (sorted_array[int(len(sorted_array) / 2)] + sorted_array[int(len(sorted_array) / 2) - 1]) / 2
+print(round(median, 1))
+
+max_freq_tuple = list(Counter(array).items())[0]
+smallest_freq_element, freq = max_freq_tuple
+
+for item in Counter(array).items():
+    if item[1] == freq:
+        smallest_freq_element = min(smallest_freq_element, item[0])
+
+mode = smallest_freq_element
+print(int(round(mode, 1)))
+
+std = math.sqrt(sum([round(math.pow(x - mean, 2), 1) for x in sorted_array]) / (len(array)))
+print(round(std, 1))
+
+stderr = std / math.sqrt(len(array))
+
+lower_limit = mean - 1.96 * stderr
+upper_limit = mean + 1.96 * stderr
+print(str(round(lower_limit, 1)) + " " + str(round(upper_limit, 1)))
