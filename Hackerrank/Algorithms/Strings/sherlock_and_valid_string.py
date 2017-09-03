3ced21b61142c03559ebfa3c6fb8ba73bbcc1257
Ahmed Hani Ibrahim <ahani.fcis@gmail.com>
2017-09-03 14:41:35 +0200

    sherlock and valid string solved

---
+++ Hackerrank/Algorithms/Strings/sherlock_and_valid_string.py
@@ -0,0 +1,32 @@
+import string
+import sys
+import copy as cp
+
+from collections import Counter
+
+
+
+def isValid(s):
+    s = list(s)
+    unique_chars = list(set(s))
+    is_valid = len(set(list(Counter(s).values()))) == 1
+    #print Counter(s)
+
+    if is_valid:
+        return "YES"
+
+    for char in unique_chars:
+        temp = "".join(cp.copy(s))
+        temp = temp.replace(char, '', 1)
+
+        is_valid = len(set(list(Counter(list(temp)).values()))) == 1
+
+        if is_valid:
+            return "YES"
+
+    return "NO"
+
+s = raw_input().strip()
+result = isValid(s)
+
+print(result)
