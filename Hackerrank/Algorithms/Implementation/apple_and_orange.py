b4d8d0997336ed3836e95c203ef9f00c7db3a12c
Ahmed Hani Ibrahim <ahani.fcis@gmail.com>
2017-09-17 23:26:11 +0200

    Create apple_and_orange.py

---
+++ Hackerrank/Algorithms/Implementation/apple_and_orange.py
@@ -0,0 +1,14 @@
+import sys
+
+
+s,t = input().strip().split(' ')
+s,t = [int(s),int(t)]
+a,b = input().strip().split(' ')
+a,b = [int(a),int(b)]
+m,n = input().strip().split(' ')
+m,n = [int(m),int(n)]
+apple = [int(apple_temp) for apple_temp in input().strip().split(' ')]
+orange = [int(orange_temp) for orange_temp in input().strip().split(' ')]
+
+print(sum([1 for num in apple if s <= a + num <= t]))
+print(sum([1 for num in orange if s <= b + num <= t]))
