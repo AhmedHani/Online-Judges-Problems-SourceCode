2270cd4c954962d74cfd13191f5b0f360791e853
Ahmed Hani Ibrahim <ahani.fcis@gmail.com>
2017-10-07 19:45:47 +0300

    Create super_reduced_string.py

---
+++ Hackerrank/Algorithms/Strings/super_reduced_string.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import sys
+
+pure = ""
+
+def super_reduced_string(s):
+    i = 1
+
+    while i < len(s):
+        if s[i] == s[i - 1]:
+            l = list(s)
+            del l[i]
+            del l[i - 1]
+            s = ''.join(l)
+            i = 0
+        else:
+            i += 1
+
+    return s if s else "Empty String"
+
+s = input().strip()
+result = super_reduced_string(s)
+print(result)
