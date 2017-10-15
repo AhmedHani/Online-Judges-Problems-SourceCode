fad549ca58b8cd3e3cfb9b4768f583f628fec85f
Ahmed Hani <ahani.fcis@gmail.com>
2017-10-16 00:44:49 +0200

    solutions added

---
+++ Hackerrank/Algorithms/Implementation/angry_professor.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import sys
+
+
+t = int(input().strip())
+for a0 in range(t):
+    n,k = input().strip().split(' ')
+    n,k = [int(n),int(k)]
+    a = [int(a_temp) for a_temp in input().strip().split(' ')]
+
+    print('YES' if sum([1 for i in a if i <= 0]) < k else "NO")
+
